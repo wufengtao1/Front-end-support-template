6ecb77dc8b137e474d1bc447429ad2ef06b26c57
wufengtao1 <760211428@qq.com>
2023-06-29 17:45:38 +0800

    Update

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-// script.js
-window.addEventListener('DOMContentLoaded', function() {
-  var datetimeElement = document.getElementById('datetime');
+// JavaScript code goes here
+function sayHello() {
+    console.log("Hello, world!");
+}
 
-  function updateDateTime() {
-    var currentDate = new Date();
-    var formattedDateTime = currentDate.toLocaleString();
-    datetimeElement.textContent = formattedDateTime;
-    }
-
-  // Initial call to display date and time
-  updateDateTime();
-
-  // Update date and time every second
-  setInterval(updateDateTime, 1000);
-});
+sayHello();
